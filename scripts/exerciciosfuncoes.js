950f11c5fbe708883db2586fbf29126641f24d3d
João Pedro Peres <britoperes@icloud.com>
2020-07-21 13:32:32 -0300

    Inclusao de novos itens nos exercicios de lista

---
+++ scripts/exerciciosfuncoes.js
@@ -0,0 +1,201 @@
+
+const  ex1=()=>{
+    //CONTRUÇÃO DA LÓGICA
+    //O exercicio consiste em digitar um numero, e o programa vai criar uma 'piramide', em que cada linha da piramide, de cima pra baixo, apresenta como numero de colunas o proprio número da linha (ex.:1/22/333)
+    //Primeiro criamos as variaveis. Uma variavel fica responsável por guardar o texto que a função vai criar. Essa variavel é a que vai guardar a 'piramide' (var texto). Outra variável vai receber o numero de linhas que a piramide terá (var numeroItens). Por ultimo, uma variavel auxiliar chamada 'controlador'. Ela terá como função limitar o numero de vezes que um numero será escrito na linha, que é o valor do proprio numero.
+    let texto = ""
+    let numeroItens = parseInt(window.prompt("Digite um numero"))
+    let controlador = 0;
+    //Com as variaveis criadas, a função primeiro verifica se o numero digitado pelo usuario é menor que o controlador. O controlador foi inicializado em zero, então ele terá como limite o próprio numero digitado.
+    const criaTexto = ()=>{
+        if(numeroItens>controlador){
+            controlador++//No momento que ele é menor, ele já é incrementado, pois determinará o numero de vezes que o numero será escrito na tela
+            for(var i = 0;i<controlador;i++){//É no for que ele atua como limitador de quantas vezes ele será escrito na variavel texto
+                texto+=`${controlador} `
+            }
+            texto+='\n'//No final do for, e ainda dentro do if, acrescentamos uma quebra de linha. Aqui a iteração acaba, tendo como produto do processo uma linha escrita de acordo com o que está definido no controlador
+            return criaTexto();//Por ultimo, a função retorna a propria função, que será executada novamente com outros valores de variáveis. Já que o determina se o IF será executado é o fato do numero digitado ser maior que o controlador, no momento em que o controlador assumir o valor do numero digitado, a ultima linha será escrita com sucesso, e o IF não terá mais nenhuma iteração.
+        }
+    }
+    criaTexto();//Uma vez que a função foi executada, a variavel que guarda o texto que será apresentado no alert abaixo já terá sido manipulada, e carregará a piramide que a função contruiu
+    alert(texto)
+    }
+//
+const  ex2=()=>{
+        let texto = ""
+        let numeroDigitado = parseInt(window.prompt("Digite o numero de linhas desejado"))
+        let controlador = 0
+        const criaPiramide = ()=>{
+            if(numeroDigitado>controlador){
+                controlador++
+                for(var i = 1;i<=controlador;i++){
+                    texto+=i+" "
+                }
+                texto+='\n'
+                return criaPiramide();
+            }
+        }
+        criaPiramide();
+        alert(texto)
+}
+
+const ex3 = ()=>{
+
+    let valor1= parseFloat(window.prompt('informe o valor 1'))
+    let valor2= parseFloat(window.prompt('informe o valor 2'))
+    let valor3= parseFloat(window.prompt('informe o valor 3'))
+
+    const soma=(soma1,soma2,soma3)=>{
+        return soma1+soma2+soma3
+    }
+
+    alert(soma(valor1,valor2,valor3));
+
+}
+//
+const ex4 = ()=>{
+    let numero = parseInt(window.prompt("Digite o numero"))
+    let valida=(num)=>{
+        if(num>0){
+            return "P"
+        } else {
+            return "N"
+        }
+    }
+    alert(valida(numero))
+}
+//
+const ex5 = ()=>{
+    let tx = parseFloat(window.prompt("Digite a taxa do imposto"))
+    let custo = parseFloat(window.prompt("Digite o custo do produto sem imposto"))
+    const somaImposto = (taxaImposto, custo)=>{
+        custo*=(1+(taxaImposto/100))
+        return custo;
+    }
+    let custoTotal = somaImposto(tx, custo)
+    alert("Custo total: "+custoTotal)
+}
+//
+const ex6 = ()=>{
+    let parar = false
+    while(parar==false){
+        let hr = parseFloat(window.prompt("Digite as horas"))
+        const conversao =(horas)=>{
+            let horasConvertidas = {
+                hora: 0,
+                medida: ""
+            }
+            
+            if(horas>=13){
+                horasConvertidas.hora = horas-12;
+                horasConvertidas.medida = "P"
+            } else if(horas>=12){
+                horasConvertidas.hora = horas;
+                horasConvertidas.medida = "P"
+            }else {
+                horasConvertidas.hora = horas;
+                horasConvertidas.medida = "A"
+              }
+              console.log(horas)
+              console.log(horasConvertidas)
+              return horasConvertidas;
+        }
+        const saida = (horasConvertidas)=>{
+            if(horasConvertidas.medida == "A"){
+                alert(horasConvertidas.hora+"AM")
+            }else{
+                alert(horasConvertidas.hora+"PM")
+            }
+        }
+        let hrConvertida = conversao(hr)
+        saida(hrConvertida)
+        let sai = window.prompt("Digite S pra continuar ou N para sair")
+        if(sai=="N"||sai=='n'){
+            parar=true
+        }
+    }
+}
+//
+const ex7 = ()=>{
+    let parar = false
+    let resultado = {
+        quantidade: 0,
+        valorTotal: 0
+    }
+    const valorPagamento = (valor, dias)=>{
+        if(dias==0){
+            resultado.quantidade++
+            resultado.valorTotal+=valor
+            return valor
+        } else {
+            let juros = dias*(valor * 0.001);
+            resultado.quantidade++
+            resultado.valorTotal+=valor
+            return (valor*1.03+juros)
+        }
+    }
+    while(parar==false){
+        let valor = window.prompt("Digite o valor da parcela")
+        if(valor==""){
+            parar=true
+        } else{
+            valor = parseFloat(valor)
+            let atraso = parseInt(window.prompt("Digite o numero de dias em atraso"))
+            alert("O valor total a ser pago é: R$"+valorPagamento(valor,atraso))
+        }
+    }
+    alert(`Relatório Final\nParcelas pagas: ${resultado.quantidade}\nValor total pago: R$${resultado.valorTotal}`)
+}
+//
+const ex8 = ()=>{
+    let numero = parseInt(window.prompt("Digite um número"))
+    const contaDigitos = (numero)=>{
+        let string = numero.toString()
+        console.log(string)
+        if(numero==0){
+            return 0
+        }else{
+            return string.length
+        }
+    }
+    alert(`O numero digitado tem ${contaDigitos(numero)} digito(s)`)
+}
+//
+const ex9 = ()=>{
+    let numero = parseInt(window.prompt("Digite um numero inteiro"))
+    const reverso = (num)=>{
+        let string = num.toString()
+        let newString = ""
+        for(var i = (string.length-1);i>=0;i--){
+            newString+=string.charAt(i)
+        }
+        return newString
+    }
+    alert(`O reverso do numero é ${reverso(numero)}`)
+}
+const ex10 = ()=>{
+    function getRandomArbitrary(min, max) { //PEGUEI NO MOZZILA
+        return Math.random() * (max - min) + min;
+      }
+      let numeroSorteado = parseInt(getRandomArbitrary(2,13))   
+      if(numeroSorteado == 2 || numeroSorteado == 3 || numeroSorteado ==12){
+          alert(`O numero sorteado foi ${numeroSorteado} e voce perdeu!`)
+      }else if(numeroSorteado == 7 ||numeroSorteado == 11){
+        alert(`O número sorteado foi ${numeroSorteado} e voce ganhou!`)
+      } else {
+          alert(`Voce sorteou o numero ${numeroSorteado} e deve continuar até encontrá-lo novamente`)
+          let tentativa = 0
+          while(numeroSorteado!=tentativa){
+              tentativa = parseInt(getRandomArbitrary(2,13))
+              if(tentativa==7){
+                  alert(`Voce perdeu pois o numero sorteado foi ${tentativa}`)
+                  tentativa=numeroSorteado
+              }else if(tentativa==numeroSorteado){
+                    alert(`Voce ganhou! O número sorteado foi novamente o ${tentativa}`)
+                    tentativa=numeroSorteado
+              }else{
+                  alert(`Tente novamente! Voce sorteou o numero ${tentativa}, e deve sortear o numero ${numeroSorteado}`)
+              }
+          }
+      }
+}
--- scripts/exercicioslista.js
+++ scripts/exercicioslista.js
@@ -57,3 +57,355 @@ const ex4 = () =>{
     alert(tc+" consoantes")
 }
 //
+const ex5 = ()=>{
+    let arrayEntrada = []
+    let contador = 0;
+    let par = [];
+    let impar = [];
+    let mensagem = "Vetor Original: "
+    while(arrayEntrada.length<10){
+        contador++
+        arrayEntrada.push(parseInt(window.prompt("Insira o "+contador+'o item')))
+        }
+    for(var i = 0;i<arrayEntrada.length;i++){
+        if((arrayEntrada[i]%2)==0){
+            par.push(arrayEntrada[i])
+        } else {
+            impar.push(arrayEntrada[i])
+        }
+    }
+    alert("O array inserido é: "+arrayEntrada)
+    alert("Os numeros pares são: "+par)
+    alert("Os numeros impares são: "+impar)
+}
+//
+const ex6 = ()=>{
+    
+
+    // const coletaNota=(arrayAluno,ct)=>{
+    //     for(var i = 0;i<4;i++){
+    //         arrayAluno.push(parseFloat(window.prompt("Insira a nota do aluno n "+ ct.nota)))
+    //     }
+    //     ct.nota ++
+    //     console.log(ct.nota)
+    //     console.log(aluno1+aluno2+aluno3+aluno4)
+    //     arrayAluno=[];
+    // }
+    // function calculaMedia(arrayAluno, novoArray){
+    //     let sum = 0
+    //     for(nota of arrayAluno){
+    //         sum+= nota;
+    //     }
+    //     novoArray.push(sum/4);
+    // }
+    // let aluno1=[]
+    // let aluno2=[]
+    // let aluno3=[]
+    // let aluno4=[]
+    // let medias=[]
+    // const contador = {nota:1}
+    // coletaNota(aluno1,contador)
+    // coletaNota(aluno2,contador)
+    // coletaNota(aluno3,contador)
+    // coletaNota(aluno4,contador)
+    // calculaMedia(aluno1, medias)
+    // calculaMedia(aluno2, medias)
+    // calculaMedia(aluno3, medias)
+    // calculaMedia(aluno4, medias)
+    // let acimaSete = 0;
+    // for(var i=0;i<medias.length;i++){
+    //     if(medias[i]>=7){
+    //         acimaSete++
+    //     }
+    // }
+    // alert(acimaSete+" alunos com nota acima ou igual a 7.0")
+    let alunos=[];
+    let medias=0;
+    for(let i = 0 ; i<4;i++){
+        for(let j = 0 ; j<4;j++){
+            const nota = parseFloat(window.prompt(`informe a nota do aluno ${i+1}`))
+            if(j==0){
+                alunos[i]=[];
+                alunos[i].push(nota)
+            }else{
+                alunos[i].push(nota)
+            } 
+        }
+    }
+
+    for(let i = 0;i<alunos.length;i++){
+        let soma=0
+        for(let j=0;j<alunos[i].length;j++){
+            soma+=alunos[i][j]
+        }
+        if(soma/4>=7){
+            medias ++;
+        }
+    }
+
+    alert(`alunos com nota maior que 7 ${medias}`)
+    
+
+}
+//
+const ex7 = ()=>{
+    let vetor = []
+    while(vetor.length<5){
+        vetor.push(parseInt(window.prompt("Insira um numero no vetor")))
+    }
+    let soma = vetor[0]
+    let multiplicacao = vetor[0]
+    for(var i =1;i<vetor.length;i++){
+        soma = soma+vetor[i]
+        multiplicacao = multiplicacao*vetor[i]
+    }
+    alert("Numeros do vetor: "+vetor+"\nSoma dos itens: "+soma+"\nMultiplicação dos itens: "+multiplicacao)
+}
+//
+const ex8 = ()=>{
+    let idade = []
+    let altura = []
+    while(idade.length<5 && altura.length<5){
+        idade.push(parseInt(window.prompt("Insira uma idade")))
+        altura.push(parseFloat(window.prompt("Insira sua respectiva altura")))
+    }
+    for(var i=4;i>=0;i--){
+        alert("Em ordem inversa:\nIdade: "+idade[i]+"\nAltura: "+altura[i])
+    }
+}
+//
+const ex9 = ()=>{
+    let array = []
+    let somaQuadrados = 0
+    while(array.length<10){
+        array.push(parseInt(window.prompt("Insira um numero caraio")))
+    }
+    for(num of array){
+        somaQuadrados = (num*num)+somaQuadrados
+    }
+    alert("A soma dos quadrados dos items é igual a "+somaQuadrados)
+}
+//
+const ex10 = ()=>{
+    let vetor1 = []
+    let vetor2 = []
+    let novoVetor = []
+    while(vetor1.length<10){
+        vetor1.push(parseFloat(window.prompt("Insira um numero no vetor 1")))
+    }
+    while(vetor2.length<10){
+        vetor2.push(parseFloat(window.prompt("Insira um numero no vetor 2")))
+    }
+
+    for(var i = 0;i<vetor1.length;i++){
+        novoVetor.push(vetor1[i])
+        novoVetor.push(vetor2[i])
+    }
+    alert("Vetor 1: "+vetor1+"\nVetor 2: "+vetor2+"\nNovo vetor: "+novoVetor)
+}
+//
+const ex11 = ()=>{
+    let vetor1 = []
+    let vetor2 = []
+    let vetor3 = []
+    let novoVetor = []
+    while(vetor1.length<10){
+        vetor1.push(parseFloat(window.prompt("Insira um numero no vetor 1")))
+    }
+    while(vetor2.length<10){
+        vetor2.push(parseFloat(window.prompt("Insira um numero no vetor 2")))
+    }
+    while(vetor3.length<10){
+        vetor3.push(parseFloat(window.prompt("Insira um numero no vetor 3")))
+    }
+
+    for(var i = 0;i<vetor1.length;i++){
+        novoVetor.push(vetor1[i])
+        novoVetor.push(vetor2[i])
+        novoVetor.push(vetor3[i])
+    }
+    alert("Vetor 1: "+vetor1+"\nVetor 2: "+vetor2+"\nVetor 3: "+vetor3+"\nNovo vetor: "+novoVetor)
+}
+//
+const ex12 = ()=>{
+    let idades = []
+    let alturas = []
+
+    while(idades.length<30){
+        idades.push(parseInt(window.prompt("Insira a idade do aluno")))
+        alturas.push(parseFloat(window.prompt("Insita a altura do aluno")))
+    }
+    let mediaAltura = 0
+    for(altura of alturas){
+        mediaAltura = mediaAltura + altura
+    }
+    mediaAltura/=alturas.length
+    let contadorInferior = 0
+    for(var i = 0;i<idades.length;i++){
+        if(idades[i]>13 && alturas[i]<mediaAltura){
+            contadorInferior++
+        }
+    }
+    alert(contadorInferior+" alunos possuem mais de 13 anos e altura inferior a media de todos os alunos")
+}
+//
+const ex13 = ()=>{
+    let meses = ["Janeiro", "Fevereiro", "Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
+    let medias = [];
+   for(var i = 0;i<meses.length;i++){
+        medias.push(parseFloat(window.prompt("Insira a temperatura média de "+meses[i])))
+    }
+    let mediaAnual = 0;
+    for(media of medias){
+        mediaAnual+= media
+    }
+    mediaAnual/= meses.length
+    console.log(meses)
+    console.log(medias)
+    console.log(mediaAnual)
+    let mesesMediaMaiorAnual = []
+    for(var i = 0; i<meses.length; i++){
+        if(medias[i]>mediaAnual){
+            mesesMediaMaiorAnual.push(meses[i])
+        }
+    }
+    alert("Os meses que apresentaram media maior que a media anual foram: "+mesesMediaMaiorAnual)
+}
+//
+const ex14 = ()=>{
+    let suspeitas = [];
+    suspeitas.push(window.prompt("Telefonou para a vítima? - S para sim e N para não"))
+    suspeitas.push(window.prompt("Esteve no local do crime? - S para sim e N para não"))
+    suspeitas.push(window.prompt("Mora perto da vítima? - S para sim e N para não"))
+    suspeitas.push(window.prompt("Devia para a vítima - S para sim e N para não"))
+    suspeitas.push(window.prompt("Já trabalhou com a vítima? - S para sim e N para não"))
+    let countSuspeitas = 0
+    for(suspeita of suspeitas){
+        if(suspeita == "S"){
+            countSuspeitas++
+        }
+    }
+    if(countSuspeitas>0&& countSuspeitas<=2){
+        alert("Suspeita")
+    } else if(countSuspeitas>2&&countSuspeitas<=4){
+        alert("Cúmplice")
+    } else if(countSuspeitas==5){
+        alert("Assassino")
+    } else {
+        alert("Inocente")
+    }
+}
+//
+const ex15=()=>{
+    let valores = []
+    let para = false
+    while(para==false){
+        let valor = parseFloat(window.prompt("Insira o valor desejado (-1 para finalizar entrada)"))
+        if(valor>=0){
+            valores.push(valor)
+        } else{
+            para=true
+        }
+    }
+    alert("Foram lidos "+valores.length+" valores")
+    alert("Os valores, na ordem em que foram lidos: "+valores)
+    let valoresAux=[]
+    for(var i = (valores.length-1);i>=0;i--){
+        valoresAux.push(valores[i])
+    }
+    alert("Os valores, na ordem inversa em que foram lidos: "+valoresAux)
+    let soma = 0;
+    for(valor of valores){
+        soma +=valor
+    }
+    alert("A soma dos valores informados é: "+soma)
+    let media = soma/valores.length
+    alert("A média dos valores é: "+media)
+    let acimaMedia = []
+    for(valor of valores){
+        if(valor>media){
+            acimaMedia.push(valor)
+        }
+    }
+    alert("Os valores acima da média são: "+acimaMedia)
+    acimaMedia = []
+    for(valor of valores){
+        if(valor>7){
+            acimaMedia.push(valor)
+        }
+    }
+    alert("Os valores acima de 7 são: "+acimaMedia)
+    alert("Final da execução do programa!")
+    console.log(valores)
+}
+//
+const ex16 = () =>{
+    let vendedores = [500, 1000,2000,3000,4000,5000,6000,7000,8000,9000,10000]
+    
+
+}
+//
+const ex17 = ()=>{
+    let parar = false
+    while(parar==false){
+        let nome = window.prompt("Digite o nome do atleta")
+        if(nome==""){
+            parar=true
+        }else{
+            let saltos = []
+            while(saltos.length<5){
+                saltos.push(parseFloat(window.prompt("Insira a distancia do salto")))
+            }
+            let message = "Atleta: "+nome
+            message+="\nPrimeiro salto: "+saltos[0]+"m"
+            message+="\nSegundo salto: "+saltos[1]+"m"
+            message+="\nTerceiro salto: "+saltos[2]+"m"
+            message+="\nQuarto salto: "+saltos[3]+"m"
+            message+="\nQuinto salto: "+saltos[4]+"m"
+            alert(message)
+            message = ""
+            message+="Resultado Final: "
+            message+="\nAtleta: "+nome
+            message+="\nSaltos: "+saltos
+            let media = 0;
+            for(salto of saltos){
+                media+=salto
+            }
+            media/=saltos.length
+            message+="\nMedia dos saltos: "+media+"m"
+            alert(message)
+
+        }
+    }
+}
+//
+const ex18 = ()=>{
+    let pontuacao = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    let parar = false;
+    while(parar==false){
+        let jogador = window.prompt("Digite o numero do jogador votado")
+        if(jogador==0){
+            parar=true
+        } else if(jogador<0 && jogador>23){
+            alert("Número inválido, digite novamente")
+        } else{
+            pontuacao[(jogador-1)]++
+        }
+    }
+
+
+    const notasJogadores=[];
+
+    pontuacao.forEach((element, index)=>{
+        if(element>0){
+        notasJogadores.push({
+            nota:element,
+            jogador:index+1
+            
+        })    
+        }
+
+    })
+
+    console.log(pontuacao)
+}
